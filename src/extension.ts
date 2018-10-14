f2bbb3cd9ba0b614e361dfd3f6fcc522af092e7b
Tadashi <kobayasi.tadashi@gmail.com>
2018-10-15 00:33:12 +0900

    add deleteLine and yank command

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,49 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "editext" is now active!');
+    var yankLine ='';
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposableDeleteLine = vscode.commands.registerCommand('extension.deleteLine', () => {
         // The code you place here will be executed every time your command is executed
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let selection = editor.selection;
+        let document = editor.document;
+        let linePos = document.lineAt(selection.active).range.start;
+        let range = new vscode.Range(linePos, linePos.translate(1));
+
+        yankLine = editor.document.getText(range);
+
+        editor.edit((edit: vscode.TextEditorEdit) => {
+            edit.delete(range);
+        });
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposableDeleteLine);
+
+    let disposableYank = vscode.commands.registerCommand('extension.yank', () => {
+        // The code you place here will be executed every time your command is executed
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        let selection = editor.selection;
+        let document = editor.document;
+        let linePos = document.lineAt(selection.active).range.start;
+
+        editor.edit((edit: vscode.TextEditorEdit) => {
+            edit.insert(linePos, yankLine);
+        });
+    });
+
+    context.subscriptions.push(disposableYank);
 }
 
 // this method is called when your extension is deactivated
