1c2ed820e75f60951ef0065cc97ac31c03b8e7b1
Tadashi <kobayasi.tadashi@gmail.com>
2018-10-15 00:36:14 +0900

    yankLine declare as string

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    var yankLine ='';
+    var yankLine :string;
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
