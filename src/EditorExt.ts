c5b3b68115942859a097445bd50a106af557c4e5
Tadashi <kobayasi.tadashi@gmail.com>
2018-10-18 21:04:24 +0900

    add delete end of line command

--- src/EditorExt.ts
+++ src/EditorExt.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 export function activateEditorExt(context: vscode.ExtensionContext) {
 	var yankString: string;
 	var yankLine: number = -1;
+	var yankStartOfLine: boolean = false;
 
 	function deleteLine() {
 		let editor = vscode.window.activeTextEditor;
@@ -12,19 +13,39 @@ export function activateEditorExt(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		let selection = editor.selection;
 		let document = editor.document;
-		let linePos = document.lineAt(selection.active).range.start;
+		let linePos = document.lineAt(editor.selection.active).range.start;
 		let range = new vscode.Range(linePos, linePos.translate(1));
 
-		let line = editor.document.getText(range);
+		let line = document.getText(range);
 
 		if (linePos.line === yankLine) {
 			yankString += line;
 		} else {
 			yankString = line;
+			yankStartOfLine = true;
 		}
 		yankLine = linePos.line;
+
+		editor.edit((edit: vscode.TextEditorEdit) => {
+			edit.delete(range);
+		});
+	}
+
+	function deleteEndOfLine() {
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		let document = editor.document;
+		let pos = editor.selection.active;
+		let lineEnd = document.lineAt(editor.selection.active).range.end;
+		let range = new vscode.Range(pos, lineEnd);
+
+		yankString = document.getText(range);
+		yankStartOfLine = false;
+		yankLine = -1;
 
 		editor.edit((edit: vscode.TextEditorEdit) => {
 			edit.delete(range);
@@ -40,10 +61,16 @@ export function activateEditorExt(context: vscode.ExtensionContext) {
 
 		let selection = editor.selection;
 		let document = editor.document;
-		let linePos = document.lineAt(selection.active).range.start;
+		var yankPos: vscode.Position;
+
+		if (yankStartOfLine) {
+			yankPos = document.lineAt(selection.active).range.start;
+		} else {
+			yankPos = selection.active;
+		}
 
 		editor.edit((edit: vscode.TextEditorEdit) => {
-			edit.insert(linePos, yankString);
+			edit.insert(yankPos, yankString);
 		});
 	}
 
@@ -53,5 +80,6 @@ export function activateEditorExt(context: vscode.ExtensionContext) {
 	}
 
 	registerCommand(context, 'deleteLine', deleteLine);
+	registerCommand(context, 'deleteEndOfLine', deleteEndOfLine);
 	registerCommand(context, 'yank', yank);
 }
